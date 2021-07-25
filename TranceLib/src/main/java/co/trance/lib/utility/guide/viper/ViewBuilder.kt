aeedee6ca3dd9ebf4c416859bf13c53da58cd502
john-hexis <jh.tech.2011@gmail.com>
2021-07-25 21:31:51 +0800

    Add set presenter and activity to view builder

--- TranceLib/src/main/java/co/trance/lib/utility/guide/viper/ViewBuilder.kt
+++ TranceLib/src/main/java/co/trance/lib/utility/guide/viper/ViewBuilder.kt
@@ -1,5 +1,9 @@
 package co.trance.lib.utility.guide.viper
 
+import android.content.Context
+
 abstract class ViewBuilder<V: IViewing> {
     abstract fun build(): V
+    abstract fun setPresenter(presenter: IPresenting)
+    abstract fun setActivity(activity: Context)
 }
