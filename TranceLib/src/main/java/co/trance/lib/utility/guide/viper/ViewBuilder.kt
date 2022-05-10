0c7efc7ced08344c3250279ab3ba5b653d5b6788
john.harries-at-378530014386 <john.harries@global.ntt>
2022-05-11 03:31:15 +0800

    Fix error on ViewBuilder

--- TranceLib/src/main/java/co/trance/lib/utility/guide/viper/ViewBuilder.kt
+++ TranceLib/src/main/java/co/trance/lib/utility/guide/viper/ViewBuilder.kt
@@ -3,8 +3,8 @@ package co.trance.lib.utility.guide.viper
 import android.content.Context
 import co.trance.lib.utility.guide.repository.IRepositoring
 
-abstract class ViewBuilder<V: IViewing<IPresenting<IInteracting<IRepositoring>>>> {
+abstract class ViewBuilder<V: IViewing<IPresenting<IInteracting>>> {
     abstract fun build(): V
-    open fun <P: IPresenting<IInteracting<IRepositoring>>> setPresenter(presenter: P): ViewBuilder<V> { throw NotImplementedError() }
+    open fun <P: IPresenting<IInteracting>> setPresenter(presenter: P): ViewBuilder<V> { throw NotImplementedError() }
     open fun setActivity(activity: Context): ViewBuilder<V> { throw NotImplementedError() }
 }
