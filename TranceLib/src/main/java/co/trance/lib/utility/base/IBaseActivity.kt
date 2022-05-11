b0ca5fecce33543b2a5c13664345cd5544576faa
john.harries-at-378530014386 <john.harries@global.ntt>
2022-05-11 17:51:07 +0800

    Add companion interface for IBaseActivity to handle action TAG parsing

--- TranceLib/src/main/java/co/trance/lib/utility/base/IBaseActivity.kt
+++ TranceLib/src/main/java/co/trance/lib/utility/base/IBaseActivity.kt
@@ -5,6 +5,9 @@ import android.location.Location
 import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.Fragment
 
+/**
+ * Interface which to be applied to [BaseActivity].
+ * */
 interface IBaseActivity {
     fun fragmentBackPressed()
     fun fragmentBackPressed(requestCode: Int, intent: Intent)
@@ -13,10 +16,25 @@ interface IBaseActivity {
     fun getDeviceLocation(success: (Location) -> Unit, failed: () -> Unit)
     fun isLocationEnabled(): Boolean
 
+    /**
+     * Interface for [Intent] action variant which to be applied to [BaseActivity].
+     * */
     interface Action {
         /**
          * Get TAG value from determined action. TAG value usually is used to make segregate action handling.
          * */
         fun getTag(): String
+
+        /**
+         * Interface for [Intent] action variant which to be applied to [BaseActivity].
+         * */
+        interface Companion<T: Action> {
+            /**
+             * Parse TAG string to [Action].
+             *
+             * @param tag string tag of [Action].
+             * */
+            fun parseTag(tag: String): T
+        }
     }
 }
