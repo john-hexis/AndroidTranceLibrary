991558c538da62fdd0d78d40984f31c5bcb17789
john.harries-at-378530014386 <john.harries@global.ntt>
2022-05-22 08:33:47 +0800

    Update ViewBuilder Generic Class

--- TranceLib/src/main/java/co/trance/lib/utility/guide/viper/ViewBuilder.kt
+++ TranceLib/src/main/java/co/trance/lib/utility/guide/viper/ViewBuilder.kt
@@ -3,7 +3,7 @@ package co.trance.lib.utility.guide.viper
 import android.content.Context
 import co.trance.lib.utility.guide.repository.IRepositoring
 
-abstract class ViewBuilder<V: IViewing<IPresenting<IInteracting>>> {
+abstract class ViewBuilder<out V: IViewing<IPresenting<IInteracting>>> {
     abstract fun build(): V
     open fun <P: IPresenting<IInteracting>> setPresenter(presenter: P): ViewBuilder<V> { throw NotImplementedError() }
     open fun setActivity(activity: Context): ViewBuilder<V> { throw NotImplementedError() }
