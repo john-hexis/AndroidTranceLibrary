eef4780efd9af721842ffc8f273ae84c3ccbbf6a
john.harries-at-378530014386 <john.harries@global.ntt>
2022-05-22 22:29:11 +0800

    Change items modifier to protected on RecyclerViewAdapter.

--- TranceLib/src/main/java/co/trance/lib/utility/helper/view/RecyclerViewAdapter.kt
+++ TranceLib/src/main/java/co/trance/lib/utility/helper/view/RecyclerViewAdapter.kt
@@ -3,7 +3,7 @@ package co.trance.lib.utility.helper.view
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class RecyclerViewAdapter<VH: RecyclerView.ViewHolder, HI: BaseHolderItem>(private var items: List<HI>): RecyclerView.Adapter<VH>() {
+abstract class RecyclerViewAdapter<VH: RecyclerView.ViewHolder, HI: BaseHolderItem>(protected var items: List<HI>): RecyclerView.Adapter<VH>() {
     /**
      * Update current items in [RecyclerView.Adapter]
      *
